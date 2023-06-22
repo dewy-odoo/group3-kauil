d58ec823e7a2b29be10f2ee62bf4d776d53a6f94
Chris Humphry <chhu@pop-os.localdomain>
2023-06-22 10:11:09 -0400

    Added battery capacity as well to the VIN

--- ge1-3362630/models/stock_lot.py
+++ ge1-3362630/models/stock_lot.py
@@ -6,6 +6,6 @@ class StockLot(models.Model):
     @api.model
     def _get_next_serial(self, company, product):
         if product.detailed_type == 'motorcycle' and product.tracking != "none":
-            return (product.make + product.model + str(product.year % 100) + str(self.env['ir.sequence'].next_by_code('stock.lot.serial')))
-        else:
-            super(StockLot, self)._get_next_serial(company, product)
+            if product.make and product.model and product.year and product.battery_capacity:
+                return (product.make + product.model + str(product.year % 100) + product.battery_capacity.upper() + str(self.env['ir.sequence'].next_by_code('stock.lot.serial')))
+        return super(StockLot, self)._get_next_serial(company, product)
