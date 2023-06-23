524f302c9e73baa7a60e1d1636696d523a2f78cb
dewy-odoo <dewy@odoo.com>
2023-06-23 13:55:58 -0400

    Added GE10 module

---
+++ ge10-3361960/__manifest__.py
@@ -0,0 +1,31 @@
+{
+    "name": "GE10",
+    
+    "summary": "GE10: Motorcycle Registry Map View",
+    
+    "description": """
+    GE10: Motorcycle Registry Map View
+    """,
+    
+    "version": "0.1",
+    
+    "category": "Kauil/Registry",
+    
+    "license": "OPL-1",
+    
+    "depends": ["motorcycle_registry", "web_map"],
+    
+    "data": [
+        "views/registry_views_inherit.xml"
+    ],
+    
+    "demo": [
+    ],
+    
+    "author": "team3",
+    
+    "website": "www.odoo.com",
+    
+    "application": False,
+    
+}
