87b7abfb3b7362544f68b2413d33a108fa4c78a5
dagu-doo <dagu@odoo.com>
2023-06-26 09:47:37 -0400

    ge4 init

---
+++ ge4-3365518/__manifest__.py
@@ -0,0 +1,32 @@
+{
+    "name": "ge4-3365518",
+    
+    "summary": "GE4: Motorcyle Repair Enhancements",
+    
+    "description": """
+    Business Case
+
+K'awiil Motors needs to manage repairs from within Odoo. The repairs app meets most of their needs, but they would like to integrate it with their motorcycle registry model. They don't want to spend time filling in information such as customer, sale order,  and products to repair. Instead, they want to be able to read the VIN, and have the rest of the information populate from the registry. They also want to be able to keep track of the motorcycle mileage each time the motorcycle is brought in for repairs and maintenance, and update the mileage on the registry with the latest value.
+    """,
+    
+    "version": "0.1",
+    
+    "category": "Kauil/Registry",
+    
+    "license": "OPL-1",
+    
+    "depends": ["motorcycle_registry", "repair"],
+    
+    "data": [
+    ],
+    
+    "demo": [
+    ],
+    
+    "author": "team3",
+    
+    "website": "www.odoo.com",
+    
+    "application": False,
+    
+}
