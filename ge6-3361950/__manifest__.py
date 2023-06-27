124ebdbfa0232206ccb72d813f1c7b8f87096315
dagu-doo <dagu@odoo.com>
2023-06-27 13:42:50 -0400

    init

---
+++ ge6-3361950/__manifest__.py
@@ -0,0 +1,30 @@
+{
+    "name": "GE6",
+    
+    "summary": "GE6: Portal Maintenance Requests",
+    
+    "description": """
+    GE6: Portal Maintenance Requests
+    """,
+    
+    "version": "0.1",
+    
+    "category": "Kauil/Registry",
+    
+    "license": "OPL-1",
+    
+    "depends": ["motorcycle_registry", "ge4-3365518"],
+    
+    "data": [
+    ],
+    
+    "demo": [
+    ],
+    
+    "author": "team3",
+    
+    "website": "www.odoo.com",
+    
+    "application": False,
+    
+}
