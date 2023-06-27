db9a0142133c472ee714cfc559b06a2fbc2935bb
dagu-doo <dagu@odoo.com>
2023-06-27 09:57:25 -0400

    ge11 completed

---
+++ ge11-3361958/__manifest__.py
@@ -0,0 +1,31 @@
+{
+    "name": "GE11",
+    
+    "summary": "GE11: Automatic Dealer Redirection",
+    
+    "description": """
+    GE11: Automatic Dealer Redirection
+    """,
+    
+    "version": "0.1",
+    
+    "category": "Kauil/Registry",
+    
+    "license": "OPL-1",
+    
+    "depends": ["motorcycle_registry", "sale"],
+    
+    "data": [
+        "views/sale_order_inherit.xml"
+    ],
+    
+    "demo": [
+    ],
+    
+    "author": "team3",
+    
+    "website": "www.odoo.com",
+    
+    "application": False,
+    
+}
